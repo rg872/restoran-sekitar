8d75bd59054888900254c9d15de2951aef291336
rg872 <gilang0293@gmail.com>
2018-04-06 01:42:41 +0700

    folder server, selesai membuat route sama fungsi buat register, login, sama delete akun

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,7 +2,7 @@ const User = require('../models/users');
 const jwt = require('jsonwebtoken');
 
 module.exports = {
-  signIn : function(req, res){
+  signInFb : function(req, res){
       let logged_user = 
       {
         email: req.body.email,
@@ -15,7 +15,7 @@ module.exports = {
         let token = jwt.sign({ email: logged_user.email, name: logged_user.name }, process.env.SECRET);
         if(user){
           res.status(200).json({
-            message:'login berhasil',
+            message:'Login succeed',
             token : token,
             status: 'login'
           });
@@ -23,7 +23,7 @@ module.exports = {
           User.create(logged_user).
           then((user)=>{
             res.status(200).json({
-              message:'user berhasil terdaftar',
+              message:'User is registered',
               token : token,
               status: 'register'
             });
@@ -33,7 +33,95 @@ module.exports = {
       })
       .catch((err)=>{
         res.status(500).json({
-          message:'server error',
+          message:'Server error',
+          err: err
+        });
+      });
+  },
+
+  register: function(req, res){
+    let register_user = 
+      {
+        email: req.body.email,
+        name: req.body.name,
+        password: req.body.password
+      }
+
+      User.findOne({ email: register_user.email })
+      .then((user)=>{
+        if(user){
+          res.status(400).json({
+            message:'Email already registered, use another email',
+            already_registered: 'true'
+          });
+        } else {
+          User.create(register_user).
+          then((user)=>{
+            res.status(200).json({
+              message:'User successfuly registered',
+              already_registered: 'false'
+            });
+          });
+        }
+        
+      })
+      .catch((err)=>{
+        res.status(500).json({
+          message:'Server error',
+          err: err
+        });
+      });
+  },
+
+  signIn: function(req, res){
+    let logged_user = 
+      {
+        email: req.body.email,
+        password: req.body.password
+      }
+
+      User.findOne({ email: logged_user.email, password: logged_user.password })
+      .then((user)=>{
+        let token = jwt.sign({ email: user.email, name: user.name }, process.env.SECRET);
+        if(user){
+          res.status(200).json({
+            message:'Succeed login',
+            token : token,
+            status: 'success'
+          });
+        } else {
+          res.status(400).json({
+            message:'Email/password is invalid',
+            status: 'fail'
+          });          
+        }
+        
+      })
+      .catch((err)=>{
+        res.status(500).json({
+          message:'Server error',
+          err: err
+        });
+      });
+  },
+
+  delete: function(req, res){
+    let deleted_user = 
+      {
+        email: req.body.email
+      }
+
+      User.deleteOne({ email: deleted_user.email })
+      .then((respone)=>{
+        res.status(200).json({
+          message:'delete user',      
+          respone:respone
+        });
+        
+      })
+      .catch((err)=>{
+        res.status(500).json({
+          message:'Server error',
           err: err
         });
       });
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 const userController = require('../controllers/userController');
 
+router.post('/signinFb', userController.signInFb);
+router.post('/register', userController.register);
 router.post('/signin', userController.signIn);
+router.delete('/delete', userController.delete);
 
 module.exports = router;
